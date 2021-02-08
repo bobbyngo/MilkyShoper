c024e19587732ddc5187a13777146c95db6513c0
bennibloo <57432484+bennibloo@users.noreply.github.com>
2021-02-08 00:58:49 -0500

    added

--- src/StoreManager.java
+++ src/StoreManager.java
@@ -1,13 +1,35 @@
 import java.util.HashMap;
 
 public class StoreManager {
+    public int getAmount(Inventory product_inv){
+        product_inv.getId_quantity();
+    }
 
+
+
+//    Inventory inventory1 = new Inventory(GoT, );
 
     /**
      * Main method
      * Calling methods and testing Product class and Inventory class will be in here
      */
     public static void main(String[] args) {
+        Product GoT = new Product("Game of Thrones: A Game of Thrones", 001, 22.00);
+
+        HashMap<Integer, Product> info_product = new HashMap<>()
+        {{
+            put(001, GoT);
+        }};
+
+        HashMap<Integer, Integer> ID_quantity = new HashMap<>()
+        {{
+            put(001, 1);
+        }};
+
+        Inventory product_inv = new Inventory(info_product, ID_quantity);
+
+//        Functionality to check the amount of Product (GoT) is in Inventory (product_inv)
+
 
     }
 }
