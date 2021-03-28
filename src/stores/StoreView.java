1ef9bf1ad8270f2f506d97ccb5fb84924f69b161
bennibloo <57432484+bennibloo@users.noreply.github.com>
2021-03-28 03:01:24 -0400

    adding GUI
    need to add:-
    + remove from cart (i.e., add to the store)
    + the checkout menu/confirmation/receipt

--- src/stores/StoreView.java
+++ src/stores/StoreView.java
@@ -10,25 +10,56 @@ package stores;// GABRIEL BENNI KELLEY EVENSEN
  * @version 1.0
  */
 
+import java.awt.image.*;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 import java.awt.*;
 import java.util.*;
+import javax.imageio.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.lang.Math;
+import javax.swing.border.*;
+import javax.swing.event.*;
 
 public class StoreView {
 
+    // Declaring the variables
     private StoreManager sm;
     private int cartID;
-    private final JFrame frame;                                     // Declaring the JFrame
-    private JLabel headerLabel;                                     // Declaring the header label
-    private JPanel mainPanel, headerPanel, bodyPanel, footerPanel;  // Declaring the required panels
-    private ArrayList<JPanel> itemsList;                           // Declaring the panels inside of the body panel
-    private JButton quitButton;                                     // Declaring the button to generate a new palette
+
+    // Declaring the frame
+    private JFrame frame;
+
+    // Declaring the panels
+    private JPanel mainPanel;
+    private JPanel headerPanel;             // Declaring the header panel will hold simply the header label
+    private JPanel bodyPanel;               // Declaring the body panel will hold the store panel and the cart panel
+    private JPanel storePanel;              // Declaring the store panel which will hold the items of the store
+    private JPanel cartPanel;               // Declaring the cart panel which will display the items in your cart
+//    private JPanel itemInStorePanel;        // Declaring the item panel which will house the information on each item in the store
+//    private JPanel itemInCartPanel;         // "" in the cart
+
+    // Declaring the header label, the items list, and the quit button
+    private JLabel headerLabel;             // Declaring the header label; "The Milky Way"
+    private JLabel storeLabel;              // Declaring the store label; "Store"
+    private JLabel cartLabel;               // Declaring the cart label; "Cart"
+    private ArrayList<JPanel> itemsList;    // Declaring the panels inside of the body panel may not be needed
+    private boolean[] itemOutOfStock;       // Declaring the array of booleans to check if an item is out of stock
+    private JButton quitBtn;                // Declaring the button to generate a new palette
+    private JButton addItemToCartBtn;       // Declaring the button to add items FROM the store TO the cart
+    private JButton addItemToStoreBtn;      // Declaring the button to REMOVE items from the cart TO the store (i.e., add to the store from the cart)
+    private JSlider addItemsToCartSld;      // Declaring the slider for how many of a certain item to add to cart
+    private JSlider addItemsToStoreSld;     // Declaring the slider for how many of a certain item to remove from the cart
+
+    GridBagConstraints c = new GridBagConstraints();
+
+
+
 
     /**
      * Constructor for the stores.StoreView
@@ -39,70 +70,117 @@ public class StoreView {
         this.sm = sm;
         this.cartID = cartID;
 
-        this.frame = new JFrame("The Milky Way");           // Initializing the JFrame
-        this.itemsList = new ArrayList<>();                    // Initializing the body panels ArrayList<JPanel>
+        storeViewGenerator();
+    }
 
-        this.headerLabel = new JLabel(
-                "Kindly select from one of our milk kinds, below:-"
-        );                                                      // Declaring and initializing the label for the header panel
+//    ##################################
+//    GUI Methods
+//    ##################################
 
-        this.mainPanel = new JPanel(new BorderLayout());        // Initializing the main panel; recommended BorderLayout
-        this.headerPanel = new JPanel();                        // Initializing the header panel
-        this.bodyPanel = new JPanel(new GridLayout());          // Initializing body panel; recommended GridLayout
-        this.footerPanel = new JPanel(new GridBagLayout());     // Initializing footer panel; recommended GridBagLayout
+    private void storeViewGenerator(){
+        this.frame = new JFrame("Milk Store");
+        this.mainPanel = new JPanel(new BorderLayout());            // Initializing the main panel; BorderLayout
+        this.headerPanel = new JPanel();                            // Initializing the header panel
+        this.bodyPanel = new JPanel(new BorderLayout());            // Initializing the body panel
+        this.storePanel = new JPanel(new BorderLayout());           // Initializing the store panel
+        this.cartPanel = new JPanel(new BorderLayout());            // Initializing the cart panel
 
-        this.quitButton = new JButton("Quit");                          // Initializing the "Generate Palette" button
-        this.quitButton.addActionListener(new ActionListener() {            // Listener attached to the button, i.e., if clicked carry out below code
+        this.headerLabel.setText("The Milky Way");                                // Initializing the header label
+        this.headerLabel.setFont(new Font("Serif", Font.BOLD, 24));     // Setting the font of the header label
+        this.storeLabel.setText("Store");                                         // Initializing the store label
+        this.storeLabel.setFont(new Font("Serif", Font.BOLD, 18));      // Setting the font of the store label
+        this.cartLabel.setText("Cart");                                           // Initializing the cart label
+        this.cartLabel.setFont(new Font("Serif", Font.BOLD, 18));       // Setting the font of the cart label
+
+        this.quitBtn = new JButton("Quit");
+        quitBtn.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {          // If the action is performed carry out below code
-                frame.setVisible(false);                                    // Setting the frame visibility of the frame to FALSE
-                frame.dispose();                                            // Disposing of the frame
+            public void actionPerformed(ActionEvent actionEvent) {
+                frame.setVisible(false);
+                frame.dispose();
             }
         });
 
     }
 
-    /**
-     * Method responsible for managing the GUI.
-     * Adding the body, header, and footer panels to the main panel.
-     * Setting up the lock panel operation.
-     * Packing and adding the main panel to the frame.
-     * Setting up frame closing menu, and setting frame visibility to TRUE.
-     */
     public void displayGUI(){
-        this.headerPanel.setPreferredSize(new Dimension(250, 100)); // Setting dimensions
-        this.footerPanel.setPreferredSize(new Dimension(250, 100)); // ""
+        this.bodyPanel.setPreferredSize(new Dimension(250, 100));
+        this.headerPanel.setPreferredSize(new Dimension(250, 100));
 
-        this.headerPanel.add(this.headerLabel);                         // Adding header label to the header panel
-        this.footerPanel.add(this.quitButton);                          // Adding the "Generate Palette" button to footer panel
+        this.headerPanel.add(this.headerLabel);
+        this.storePanel.add(this.storeLabel);
+        this.cartPanel.add(this.cartLabel);
 
-        this.mainPanel.add(this.headerPanel, BorderLayout.PAGE_START);  // Adding the header panel to the main panel; at the page start
-        this.mainPanel.add(this.bodyPanel, BorderLayout.CENTER);        // Adding the body panel to the main panel; at the page centre
-        this.mainPanel.add(this.footerPanel, BorderLayout.PAGE_END);    // Adding the footer panel to the main panel; at the page end
+        createStorePanels();
 
-        this.frame.add(this.mainPanel);                                 // Adding the main panel to the frame
-        this.frame.pack();                                              // Packing the frame
+    }
 
-        this.frame.addWindowListener(new WindowAdapter() {                                          // Listener attached to the frame, i.e., if close clicked carry out below code
-            @Override
-            public void windowClosing(WindowEvent we) {                                             // When the action is that of a window closing carry out below code
-                if (JOptionPane.showConfirmDialog(frame, "Are you sure you want to quit?")  // If the option selected is okay carry out below code
-                        == JOptionPane.OK_OPTION) {
-                    // close it down!
-                    frame.setVisible(false);                                                        // Setting the frame visibility of the frame to FALSE
-                    frame.dispose();                                                                // Disposing of the frame
+    private void createStorePanels(){
+
+        InputStream imageStream = this.getClass().getResourceAsStream("src/stores/milk.jpg"); // Getting the milk image
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(imageStream);                            // Assigning to image
+        } catch (IOException e) {                                         // Catch for I/O exceptions
+            e.printStackTrace();                                          // Print error to StackTrace
+        }
+        JLabel picLabel = new JLabel(new ImageIcon(image));               // Assigning the image to a JLabel
+        picLabel.setPreferredSize(new Dimension(30, 30));     // Setting the preferred image dimension
+
+        for (Integer i : sm.getKeySet()) {
+            JPanel itemInStorePanel = new JPanel(new GridBagLayout());
+            itemInStorePanel.setPreferredSize(new Dimension(80, 100));
+
+            c.gridx = 0;
+            c.gridy = 0;
+            itemInStorePanel.add(picLabel, c);
+
+            c.gridx = 1;
+            c.gridy = 0;
+            itemInStorePanel.add(new JLabel(sm.getProduct().get(i).getName() + " at " +
+                    sm.getProduct().get(i).getPrice() + "$/unit"), c);
+
+            c.gridx = 0;
+            c.gridy = 1;
+            JSlider sld = new JSlider(JSlider.HORIZONTAL, 0, sm.getQuantity().get(i), 0);
+            // Change listener on the slider
+            // When the slider is moved to some integer x, store x in the 'value' variable
+            final int[] value = {0};
+            sld.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent changeEvent) {
+                    JSlider slider = (JSlider) changeEvent.getSource();
+                    if (!slider.getValueIsAdjusting()) {    // If the slider is not currently moving carry out the below
+                        value[0] = slider.getValue();
+                    }
                 }
-            }
-        });
-        this.frame.setVisible(true);                                                                // Setting the frame visibility of the frame to TRUE
+            });
+            itemInStorePanel.add(sld, c);   // Adding the slider to the item panel
+
+            c.gridx = 1;
+            c.gridy = 1;
+            JButton btn = new JButton("Add to cart");
+            // Action listener on the "Add to cart" button
+            // When an action (i.e., press) is performed on the button remove the 'value' amount (described in the
+            // above ChangeListener) of the current product, from the cart with the corresponding cartID.
+            btn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    try {
+                        sm.removeCartInventory(sm.getProduct().get(i).getId(), value[0], cartID);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            itemInStorePanel.add(btn, c);   // Adding the button to the item panel
+        }
     }
 
-    /*
-     * Method reponsible for managing the buttons that lock the body panels; i.e., the colour changing panels
-     * */
-    public void storeAddRemoveOperation(){
-        
-    }
+
+//    ##################################
+//    Console Methods
+//    ##################################
 
     /**
      * The helpDisplay method will give the information about the command lines
@@ -115,58 +193,58 @@ public class StoreView {
      * The browseDisplay will print out all the information about the products that exist in the inventory
      * with the give format
      */
-    private void browseDisplay() {
-        System.out.println("|----------THE COURSE STORE----------|");
-        System.out.println("\\---------------BROWSE---------------/\n");
-        System.out.println("Stock | ID | stores.Product Name | Unit Price");
-
-        for (Integer i : sm.getKeySet()) {
-            System.out.println(sm.getQuantity().get(i) + " | "
-                    + sm.getProduct().get(i).getId() + " | "
-                    + sm.getProduct().get(i).getName() + " | "
-                    + "$" + sm.getProduct().get(i).getPrice());
-        }
-    }
+//    private void browseDisplay() {
+//        System.out.println("|----------THE COURSE STORE----------|");
+//        System.out.println("\\---------------BROWSE---------------/\n");
+//        System.out.println("Stock | ID | stores.Product Name | Unit Price");
+//
+//        for (Integer i : sm.getKeySet()) {
+//            System.out.println(sm.getQuantity().get(i) + " | "
+//                    + sm.getProduct().get(i).getId() + " | "
+//                    + sm.getProduct().get(i).getName() + " | "
+//                    + "$" + sm.getProduct().get(i).getPrice());
+//        }
+//    }
 
     /**
      * The addDisplay will be called when the user want to add the items into their cart. This methods will called the removeCartInventory
      * method in the stores.StoreManager class which will add the items into their cart and remove the items from the stores.Inventory.
      */
-    private void addDisplay() {
-        System.out.println("|----------THE COURSE STORE----------|");
-        System.out.println("\\----------------ADD----------------/\n");
-        System.out.println("Stock | ID | stores.Product Name | Unit Price ");
+//    private void addDisplay() {
+//        System.out.println("|----------THE COURSE STORE----------|");
+//        System.out.println("\\----------------ADD----------------/\n");
+//        System.out.println("Stock | ID | stores.Product Name | Unit Price ");
+//
+//        for (Integer i : sm.getKeySet()) {
+//            System.out.println(sm.getQuantity().get(i) + " | "
+//                    + sm.getProduct().get(i).getId() + " | "
+//                    + sm.getProduct().get(i).getName() + " | "
+//                    + "$" + sm.getProduct().get(i).getPrice());
+//        }
+//        System.out.println("\nPlease choose the ID of the stores.Product to add");
+//        Scanner myObj = new Scanner(System.in);
+//
+//        try {
+//            int id = myObj.nextInt();
+//            if (sm.getQuantity().containsKey(id)) {
+//                System.out.println("\nPlease choose the amount want to add");
+//                myObj = new Scanner(System.in);
+//                int quantity = myObj.nextInt();
+//
+//                sm.removeCartInventory(id, quantity, cartID);
+//
+//            }else {
+//                System.out.println("\nYour ID is not available " +
+//                        "\nPlease choose the available ID");
+//            }
+//        }catch (Exception e) {
+//            System.out.println(e);
+//            System.out.println("Please enter valid choice \n");
+//        }
+//
+//    }
 
-        for (Integer i : sm.getKeySet()) {
-            System.out.println(sm.getQuantity().get(i) + " | "
-                    + sm.getProduct().get(i).getId() + " | "
-                    + sm.getProduct().get(i).getName() + " | "
-                    + "$" + sm.getProduct().get(i).getPrice());
-        }
-        System.out.println("\nPlease choose the ID of the stores.Product to add");
-        Scanner myObj = new Scanner(System.in);
-
-        try {
-            int id = myObj.nextInt();
-            if (sm.getQuantity().containsKey(id)) {
-                System.out.println("\nPlease choose the amount want to add");
-                myObj = new Scanner(System.in);
-                int quantity = myObj.nextInt();
-
-                sm.removeCartInventory(id, quantity, cartID);
-
-            }else {
-                System.out.println("\nYour ID is not available " +
-                        "\nPlease choose the available ID");
-            }
-        }catch (Exception e) {
-            System.out.println(e);
-            System.out.println("Please enter valid choice \n");
-        }
-
-    }
-
-    /**
+    /** YET TO IMPLEMENT
      * The removeDisplay will be called when the user want to remove items from their cart. This methods will called the addCartInventory
      * method in the stores.StoreManager class which will remove the items from their cart and add the items back to the stores.Inventory.
      */
@@ -211,7 +289,7 @@ public class StoreView {
 
     }
 
-    /**
+    /** YET TO IMPLEMENT
      * The checkOutDisplay will print out the information of the products in the customer's cart with the total price
      */
     private void checkOutDisplay() {
@@ -247,11 +325,11 @@ public class StoreView {
         }
 
         if (demand.equals("browse")){
-            browseDisplay();
+//            browseDisplay();
         }
 
         if (demand.equals("add")){
-            addDisplay();
+//            addDisplay();
         }
 
         if (demand.equals("remove")){
